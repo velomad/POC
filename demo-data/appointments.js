f6f3328b4b1e51208c9f0b08994222085aded00f
Ronil <ronilraut005@gmail.com>
2021-09-07 09:45:37 +0530

    sheduler added

---
+++ demo-data/appointments.js
@@ -0,0 +1,57 @@
+export const appointments = [
+    {
+      title: ' Rupesh Rao	',
+      startDate: new Date(2021, 8, 6, 10, 30),
+      endDate: new Date(2021, 8, 6, 11, 0),
+      id: 0,
+      patientContactNumber:"9989765278"
+    }, {
+      title: 'Pradyot Rath',
+      startDate: new Date(2021, 7, 29, 12, 0),
+      endDate: new Date(2021, 7, 29, 12, 30),
+      id: 1,
+      patientContactNumber:"8789165678"
+
+    }, {
+      title: 'Vanzara',
+      startDate: new Date(2021, 8, 6, 9, 30),
+      endDate: new Date(2021, 8, 1, 10, 0),
+      id: 2,
+      patientContactNumber:"8889705678"
+
+    }, {
+      title: 'Vanzara',
+      startDate: new Date(2021, 8, 2, 13, 0),
+      endDate: new Date(2021, 8, 2, 13, 30),
+      id: 3,
+      patientContactNumber:"7789765678"
+
+    }, {
+      title: 'Pradyot Rath',
+      startDate: new Date(2021, 6, 1, 12, 0),
+      endDate: new Date(2021, 6, 1, 12, 30),
+      id: 4,
+      patientContactNumber:"9789705678"
+
+    }, {
+      title: ' Ajay',
+      startDate: new Date(2021, 8, 3, 11, 0),
+      endDate: new Date(2021, 8, 3, 11, 30),
+      id: 18,
+      patientContactNumber:"6789745678"
+
+    }, {
+      title: 'Pradyot Rath',
+      startDate: new Date(2021, 8, 4, 12, 0),
+      endDate: new Date(2021, 8, 4, 12, 30),
+      id: 6,
+      patientContactNumber:"9789765278"
+
+    }, {
+      title: ' Ajay',
+      startDate: new Date(2021, 7, 29, 13, 0),
+      endDate: new Date(2021, 7, 29, 13, 30),
+      id: 7,
+      patientContactNumber:"8789765178"
+
+    }]
