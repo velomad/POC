22fd5ac211281c00882304fb27629b13fedc55c3
Ajay 007 <ajaybendre@credenceanalytics.com>
2021-09-09 17:45:31 +0530

    changes merged

--- client/src/Pages/Scheduler.js
+++ client/src/Pages/Scheduler.js
@@ -149,7 +149,7 @@ const Scheduler = () => {
             </div>
           </div>
         </div>
-        <AppointmentScheduler />
+        {/* <AppointmentScheduler /> */}
       </div>
     </div>
   );
